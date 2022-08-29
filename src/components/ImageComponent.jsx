78f74525fc92a426c143d4d44af82b625b3f7c64
Kappi90 <you@example.com>
2022-08-29 16:08:15 +0200

    First Commit

---
+++ src/components/ImageComponent.jsx
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+
+class ImageComponent extends Component{
+    render(){
+        return <div><img src={this.props.src} alt={this.props.alt} /></div>
+    }
+}
+export default ImageComponent;
